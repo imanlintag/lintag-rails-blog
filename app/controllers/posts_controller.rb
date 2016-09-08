e8dea6697fe2fa0aea6599e1bda068943fdf34f7
[myblog] <imanlintag13@mittymonarch.com>
2016-09-08 16:56:10 -0700

    Add user validation and user id to comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,60 @@
 class PostsController < ApplicationController
-	before_filter :login_required, :except => [:index, :show]
-
-	def index 
-		@posts = Post.all.order('created_at DESC')
-	end
-
-	def new
-		@post = Post.new
-	end
-
-	def create
-		@post = Post.new(post_params)
-
-		if @post.save
-			redirect_to @post
-		else 
-			render 'new'
-		end
-	end
-
-	def show
-		@post = Post.find(params[:id])
-	end
-
-	def edit 
-		@post = Post.find(params[:id])
-	end
-
-	def update
-		@post = Post.find(params[:id])
-
-		if @post.update(params[:post].permit(:title, :body))
-			redirect_to @post 
-		else
-			render 'edit'
-		end
-	end
-
-	def destroy
-		@post = Post.find(params[:id])
-		@post.destroy
-
-		redirect_to root_path
-	end
-
-	private
-		def post_params
-			params.require(:post).permit(:title, :body)
-	end
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user,       only: :destroy
+  
+    def index
+      @post = Post.all.order('created_at DESC')
+    end
+    
+    def new
+      @post = Post.new
+    end
+    
+    def create
+      @user = current_user
+      @post = @user.posts.new(post_params)
+      
+      if @post.save
+        redirect_to @post
+      else
+        render 'new'
+      end
+    end
+    
+    def show
+      @post = Post.find(params[:id])
+    end
+    
+    def edit
+      @post = Post.find(params[:id])
+    end
+    
+    def update
+      @post = Post.find(params[:id])
+      
+      if @post.update(post_params)
+        redirect_to @post
+      else
+        render 'edit'
+      end
+    end
+    
+    def destroy
+      @post.destroy
+      redirect_to root_path
+    end
+    
+    
+    private
+      def post_params
+        params.require(:post).permit(:title, :body)
+      end
+      
+      def correct_user
+        @post = current_user.posts.find_by(id: params[:id])
+        if @post.nil?
+          flash[:alert] = "Not your post!"
+          redirect_to root_path
+        end
+      end
 end
