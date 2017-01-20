ca95273bfb692fac8be9b63d4beb47236204ea3e
[myblog] <imanlintag13@mittymonarch.com>
2017-01-19 19:50:51 -0800

    Update Resume

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
-	belongs_to :user
+	belongs_to :user, required: true
 end
